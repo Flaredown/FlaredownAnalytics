b055060f0ccbf1a5e1e375c05fce03fc613335cc
Colin Gorrie <colin@colingorrie.com>
2015-09-07 11:49:10 -0400

    Return nice JSON for Entry

--- fdanalytics/app.py
+++ fdanalytics/app.py
@@ -11,9 +11,29 @@ db = connect(app.config)
 api = Api(app)
 
 
-class TreatmentAPI(Resource):
-    def get(self, name):
+class EntryAPI(Resource):
+    def get(self):
         entry = db.entries.find_one()
-        return {"response": to_json(entry)}
+        return {
+            "_id": to_json(entry["_id"]),
+            "user_id": entry["user_id"],
+            "date": to_json(entry["date"]),
+            "settings": entry["settings"],
+            "conditions": entry["conditions"],
+            "symptoms": entry["symptoms"],
+            "responses": [{
+                "name": response["name"],
+                "value": response["value"],
+                "catalog": response["catalog"],
+            } for response in entry["responses"]],
+            "conditions": entry["conditions"],
+            "symptoms": entry["symptoms"],
+            "treatments": [{
+                "name": treatment["name"],
+                "quantity": treatment["quantity"],
+                "unit": treatment["unit"],
+                "repetition": treatment["repetition"],
+            } for treatment in entry["treatments"]],
+        }
 
-api.add_resource(TreatmentAPI, "/analytics/api/v1.0/treatment/<name>")
+api.add_resource(EntryAPI, "/analytics/api/v1.0/entry")
