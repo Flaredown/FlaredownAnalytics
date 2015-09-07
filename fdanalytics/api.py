c0a488c7013218b89e95f578951a6c2d6b287dc6
Colin Gorrie <colin@colingorrie.com>
2015-09-07 10:50:06 -0400

    Add ability to connect to mongodb

--- fdanalytics/api.py
+++ fdanalytics/api.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_restful import Api
+from pymongo import MongoClient
 from .resources.treatment import TreatmentAPI
 
 
 app = Flask(__name__)
+app.config.from_pyfile("../config.py")
+
+client = MongoClient("mongodb://{}:{}".format(
+    app.config["MONGO_HOST"],
+    app.config["MONGO_PORT"]))
+
 api = Api(app)
 
 api.add_resource(TreatmentAPI, "/analytics/api/v1.0/treatment/<name>")
