381a999a9e39d9f761adb2b8d8fb48ab127ae7c9
Colin Gorrie <colin@colingorrie.com>
2015-09-10 12:20:21 -0400

    Connect to both remote and local db

--- fdanalytics/common/db.py
+++ fdanalytics/common/db.py
@@ -2,9 +2,19 @@ from pymongo import MongoClient
 
 
 def connect(config):
-    client = MongoClient("mongodb://{}:{}".format(
-        config["MONGO_HOST"],
-        config["MONGO_PORT"]))
-    db = client[config["DB_NAME"]]
+    if config["USER_ID"] and config["PASSWORD"]:
+        client = MongoClient("mongodb://{}:{}@{}:{}/{}".format(
+            config["USER_ID"],
+            config["PASSWORD"],
+            config["MONGO_HOST"],
+            config["MONGO_PORT"],
+            config["DB_NAME"]))
+    else:
+        client = MongoClient("mongodb://{}:{}/{}".format(
+            config["MONGO_HOST"],
+            config["MONGO_PORT"],
+            config["DB_NAME"]))
+
+    db = client.get_default_database()
 
     return db
