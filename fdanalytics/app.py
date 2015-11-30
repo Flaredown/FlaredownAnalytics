7a8a51b769632504c2c3bda780b73bd125805999
Colin Gorrie <colin@colingorrie.com>
2015-11-30 16:43:20 -0500

    Get rid of old database connection code, clean up multiple environment config

--- fdanalytics/app.py
+++ fdanalytics/app.py
@@ -1,9 +1,8 @@
+import os
 from flask import Flask, Blueprint, g
 from flask_restful import Api
-from flask.ext.heroku import Heroku
 from flask.ext.pymongo import PyMongo
 from werkzeug.contrib.fixers import ProxyFix
-from .common.db import configure_client, connect
 from .resources.root import RootAPI
 from .resources.condition import ConditionListAPI
 from .resources.entry import EntryAPI, EntryListAPI
@@ -12,16 +11,30 @@ from .resources.symptom import SymptomListAPI
 from .resources.treatment import TreatmentAPI, TreatmentListAPI
 from .resources.user import UserAPI
 
-# client = configure_client(app.config)
-# db = connect(client)
 
-heroku = Heroku()
+class Config(object):
+    pass
+
+
+class HerokuConfig(Config):
+    MONGO_HOST = os.environ.get("MONGO_HOST")
+    MONGO_PORT = os.environ.get("MONGO_PORT")
+    MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
+    MONGO_USERNAME = os.environ.get("MONGO_USERNAME")
+    MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
+
+
 mongo = PyMongo()
 
 
 def create_app(config_filename):
     app = Flask(__name__)
-    app.config.from_pyfile(config_filename)
+
+    if os.environ.get("HEROKU"):
+        app.config.from_object(HerokuConfig)
+    else:
+        app.config.from_pyfile("../config.py")
+
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
     api_bp = Blueprint("api", __name__)
@@ -39,40 +52,12 @@ def create_app(config_filename):
 
     app.register_blueprint(api_bp)
 
-    heroku.init_app(app)
     mongo.init_app(app)
 
     return app
 
+
 app = create_app("../config.py")
-
-# app = Flask(__name__)
-# app.config.from_pyfile("../config.py")
-# app.wsgi_app = ProxyFix(app.wsgi_app)
-
-# client = configure_client(app.config)
-# db = connect(client)
-
-# api_bp = Blueprint("api", __name__)
-# api = Api(api_bp)
-
-# api.add_resource(RootAPI, "/analytics/api/v1.0")
-
-# api.add_resource(ConditionListAPI, "/analytics/api/v1.0/conditions/")
-
-# api.add_resource(EntryListAPI, "/analytics/api/v1.0/entries/")
-# api.add_resource(EntryAPI, "/analytics/api/v1.0/entries/<entry_id>")
-
-# api.add_resource(SymptomListAPI, "/analytics/api/v1.0/symptoms/")
-
-# api.add_resource(TreatmentListAPI, "/analytics/api/v1.0/treatments/")
-# api.add_resource(TreatmentAPI, "/analytics/api/v1.0/treatments/<treatment_name>")
-
-# api.add_resource(UserAPI, "/analytics/api/v1.0/users/<user_id>")
-
-# api.add_resource(SegmentAPI, "/analytics/api/v1.0/segments")
-
-# app.register_blueprint(api_bp)
 
 
 @app.before_request
