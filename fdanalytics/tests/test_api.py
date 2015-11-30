afb24ae213f30f463fdb00c739b38c7e36523b22
Colin Gorrie <colin@colingorrie.com>
2015-11-30 15:52:05 -0500

    Get all endpoints under test

--- fdanalytics/tests/test_api.py
+++ fdanalytics/tests/test_api.py
@@ -29,9 +29,63 @@ class ConditionListTest(FD_AnalyticsLiveServerTestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class EntryTest(FD_AnalyticsLiveServerTestCase):
+    def setUp(self):
+        self.url = self._namespace() + "/entries/5630e2539ef2f4d01d0ee0a7"
+
+    def test_get_returns_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class EntryListTest(FD_AnalyticsLiveServerTestCase):
+    def setUp(self):
+        self.url = self._namespace() + "/entries"
+
+    def test_get_returns_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
 class SegmentTest(FD_AnalyticsLiveServerTestCase):
     def setUp(self):
         self.url = self._namespace() + "/segments"
+
+    def test_get_returns_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class SymptomListTest(FD_AnalyticsLiveServerTestCase):
+    def setUp(self):
+        self.url = self._namespace() + "/symptoms"
+
+    def test_get_returns_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TreatmentTest(FD_AnalyticsLiveServerTestCase):
+    def setUp(self):
+        self.url = self._namespace() + "/treatments/caffeine"
+
+    def test_get_returns_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TreatmentListTest(FD_AnalyticsLiveServerTestCase):
+    def setUp(self):
+        self.url = self._namespace() + "/treatments"
+
+    def test_get_returns_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class UserTest(FD_AnalyticsLiveServerTestCase):
+    def setUp(self):
+        self.url = self._namespace() + "/users/11"
 
     def test_get_returns_200(self):
         response = requests.get(self.url)
